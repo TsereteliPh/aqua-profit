1f04b317829b64a93b46404b7f6dcfd8145d1bd6
Tsereteli.Ph <filipptsereteli@yandex.ru>
2023-07-28 09:34:51 +0300

    404 page changes

--- 404.php
+++ 404.php
@@ -1,11 +1,11 @@
 <?php get_header(); ?>
 
 <section class="not-found">
-	<div class="container not-found__container">
+	<div class="container not-found__wrapper">
 		<div class="not-found__text">
-			<h1 class="not-found__title">СТРАНИЦА НЕ НАЙДЕНА</h1>
-			<p>Такой страницы не существует.</p>
-			<p>Воспользуйтесь поиском по сайту.</p>
+			<h1 class="main-caption not-found__title">Ошибка 404</h1>
+			<p>К сожалению такой страницы не существует.</p>
+			<a href="<?php bloginfo('url'); ?>" class="not-found__link">Вернуться на гланую</a>
 		</div>
 	</div>
 </section>
