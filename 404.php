7c623855814330c509818d2140554289b0a1c50b
Tsereteli.Ph <filipptsereteli@yandex.ru>
2023-08-04 13:52:30 +0300

    done 404 page

--- 404.php
+++ 404.php
@@ -3,9 +3,9 @@
 <section class="not-found">
 	<div class="container not-found__wrapper">
 		<div class="not-found__text">
-			<h1 class="main-caption not-found__title">Ошибка 404</h1>
+			<h1 class="title not-found__title">Ошибка 404</h1>
 			<p>К сожалению такой страницы не существует.</p>
-			<a href="<?php bloginfo('url'); ?>" class="not-found__link">Вернуться на гланую</a>
+			<a href="<?php bloginfo('url'); ?>" class="btn-underline not-found__link">Вернуться на гланую</a>
 		</div>
 	</div>
 </section>
