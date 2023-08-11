7cacea4ae9a20c4e56c132d656147aab0afaaa4a
Tsereteli.Ph <filipptsereteli@yandex.ru>
2023-08-11 11:01:56 +0300

    update 404

--- 404.php
+++ 404.php
@@ -5,7 +5,7 @@
 		<div class="not-found__text">
 			<h1 class="title not-found__title">Ошибка 404</h1>
 			<p>К сожалению такой страницы не существует.</p>
-			<a href="<?php bloginfo('url'); ?>" class="btn-underline not-found__link">Вернуться на гланую</a>
+			<a href="<?php bloginfo('url'); ?>" class="btn-underline not-found__link">Вернуться на главную</a>
 		</div>
 	</div>
 </section>
