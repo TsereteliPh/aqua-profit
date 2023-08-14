ae97d5fdadf6b9d5df75d2369391781036d2cb53
Tsereteli.Ph <filipptsereteli@yandex.ru>
2023-08-14 11:45:46 +0300

    update reviews

--- layouts/partials/cards/review.php
+++ layouts/partials/cards/review.php
@@ -1,36 +1,48 @@
 <?php
+	$is_project = get_field( 'is_project' );
 	$text = get_field( 'review_text' );
-	$title = get_the_title();
-?>
-<li class="review-card <?php echo $args['class']; ?>">
-	<?php
+
+	if ( $is_project ) {
 		$post = get_field( 'review_project' );
 
-		if ( $post ) :
-		setup_postdata( $post );
-	?>
-		<div class="review-card__project">
-			<?php
-				if ( get_the_post_thumbnail() ) {
-					echo the_post_thumbnail( 'medium', array(
-						'class' => 'review-card__img'
-					) );
-				} else {
-					echo wp_get_attachment_image( 107, 'medium', false, array(
-						'class' => 'review-card__img'
-					) );
-				}
-			?>
+		if ( $post ) {
+			setup_postdata( $post );
+				$img = get_post_thumbnail_id();
+				$projectTitle = get_the_title();
+				$projectLink = get_the_permalink();
+				$works = get_field( 'works_list' );
+			wp_reset_postdata();
+		}
+	} else {
+		$img = get_field( 'review_img' );
+		$works = get_field( 'review_works' );
+	}
+?>
+<li class="review-card <?php echo $args['class']; ?>">
+	<div class="review-card__project">
+		<?php
+			if ( $img ) {
+				echo wp_get_attachment_image( $img, 'medium', false, array(
+					'class' => 'review-card__img'
+				) );
+			} else {
+				echo wp_get_attachment_image( 107, 'medium', false, array(
+					'class' => 'review-card__img'
+				) );
+			}
+		?>
 
-			<div class="review-card__info">
-				<div class="review-card__name"><?php echo $title; ?></div>
+		<div class="review-card__info">
+			<div class="review-card__name"><?php the_title(); ?></div>
 
-				<a href="<?php the_permalink(); ?>" class="review-card__label"><?php the_title(); ?></a>
+			<?php if ( $projectLink ) : ?>
+				<a href="<?php echo $projectLink; ?>" class="review-card__label"><?php echo $projectTitle; ?></a>
+			<?php endif; ?>
 
+			<?php if ( $works ) : ?>
 				<div class="review-card__works">
 					<span>Выполненные работы:</span>
 					<?php
-						$works = get_field( 'works_list' );
 						$worksCount = count($works);
 
 						foreach ( $works as $key => $work ) {
@@ -38,12 +50,11 @@
 						}
 					?>
 				</div>
-			</div>
+			<?php endif; ?>
 		</div>
+	</div>
 
+	<?php if ( $text ) : ?>
 		<div class="review-card__text"><?php echo $text; ?></div>
-	<?php
-		wp_reset_postdata();
-		endif;
-	?>
+	<?php endif; ?>
 </li>
